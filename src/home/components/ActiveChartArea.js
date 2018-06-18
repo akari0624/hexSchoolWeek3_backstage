9c46798c6930f34116555acd431429bd21a6e5ac
akari0624 <akari0624@hotmail.com>
2018-06-19 01:48:10 +0800

    construct d3 up

--- src/home/components/ActiveChartArea.js
+++ src/home/components/ActiveChartArea.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Styled from 'styled-components'
 import PropTypes from 'prop-types'
-
+import { select, line, scaleLinear, scaleTime } from 'd3' 
 
 
 const Wrapper = Styled.section`
@@ -18,13 +18,58 @@ const AreaTitle = Styled.div`
   color: #000000;
 `
 
+const D3LineChartAreaWrapper = Styled.div`
+  margin:5px 30px 30px 30px;
 
-const ActiveChartArea = props => (
+`
 
-  <Wrapper>
-    <AreaTitle>Activity</AreaTitle>
-  </Wrapper>
-)
+const D3ChartSVG = Styled.svg`
+  height:100%;
+  width:100%;
+`
+
+
+const drawInitLineChart = (svgRef, data) => {
+
+  const lineFunc = line().x(d => d.x).y(d => d.y)
+
+  const lineGWrapper = select(svgRef).append('g')
+
+
+  lineGWrapper.append('path').datum(data).attr('fill','none')
+    .attr('stroke','steelblue')
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-linecap', 'round')
+    .attr('stroke-width', 1.5)
+    .attr('d',lineFunc)
+  
+
+}
+
+
+class ActiveChartArea extends Component{
+
+
+  render(){
+    return(
+      <Wrapper>
+        <AreaTitle>Activity</AreaTitle>
+        <D3LineChartAreaWrapper>
+          <D3ChartSVG innerRef={(tRef) => this.svgRef = tRef} />
+        </D3LineChartAreaWrapper>
+      </Wrapper>
+    )
+  }
+
+
+  componentDidMount(){
+
+
+    const lineData = [{x:5,y:5},{x:8,y:8},{x:10,y:10},{x:15,y:15},{x:20,y:20},]
+    drawInitLineChart(this.svgRef, lineData)
+
+  }
+}
 
 
 ActiveChartArea.propTypes = {
