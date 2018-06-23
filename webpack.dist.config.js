9efe3f3f718cdf92c20eddd60066e95e5e433239
akari0624 <akari0624@hotmail.com>
2018-06-24 02:23:18 +0800

    deploy to ghPage

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -7,7 +7,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/asset',
+        publicPath: '',
         filename: 'bundle.js'
     },
 
