9dd03015479040b223ef26107040db2103f2a319
akari0624 <akari0624@hotmail.com>
2018-06-24 21:28:45 +0800

    use antd dropdown_menu

--- src/home/components/PageTitleBarWithDateFilterDropDown.js
+++ src/home/components/PageTitleBarWithDateFilterDropDown.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { DatePicker } from 'antd';
+import moment from 'moment';
+const { RangePicker } = DatePicker;
+import 'antd/lib/date-picker/style'
+import { Menu, Dropdown, Icon, message } from 'antd';
+import 'antd/lib/dropdown/style'
+import 'antd/lib/message/style'
 
 
 
@@ -21,12 +28,43 @@ const DateFilterWrapper = Styled.div`
 
 `
 
+const DropdownMenuWrapper = Styled.div`
+`
+
+const dateFormat = 'YYYY/MM/DD';
+
+
+const handleDropdownMenuClick = ({key}) => {
+
+  console.log(`you click ${key}`)
+  message.info(`you click ${key}`)
+
+}
+
+const menu = (
+  <Menu onClick={handleDropdownMenuClick}>
+    <Menu.Item key="day">Daily</Menu.Item>
+    <Menu.Item key="week">Weekly</Menu.Item>
+    <Menu.Item key="month">Monthly</Menu.Item>
+    <Menu.Item key="year">Yearly</Menu.Item>
+    <Menu.Item key="custom">custom</Menu.Item>
+  </Menu>
+);
+
 
 const PageTitleBarWithDateFilterDropDown = props => (
 
   <Wrapper>
     <PageTitle> OVERVIEW </PageTitle>
-    <DateFilterWrapper> 日期過濾區塊 </DateFilterWrapper>
+    <DateFilterWrapper> <RangePicker defaultValue={[moment('2018/06/01', dateFormat), moment('2018/06/15', dateFormat)]}
+      format={dateFormat}/> </DateFilterWrapper>
+    <DropdownMenuWrapper>
+      <Dropdown overlay={menu}>
+        <a className="ant-dropdown-link" href="#">
+          產生條件 <Icon type="down" />
+        </a>
+      </Dropdown>  
+    </DropdownMenuWrapper>  
   </Wrapper>
 
 
